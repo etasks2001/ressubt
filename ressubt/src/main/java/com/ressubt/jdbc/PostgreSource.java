871d73602d41fbf20dbd6da4a2640d62f02e4280
mauro <mauro@mauro-PC>
2020-11-05 10:50:24 -0300

    package jdbc

---
+++ ressubt/src/main/java/com/ressubt/jdbc/PostgreSource.java
@@ -0,0 +1,12 @@
+package com.ressubt.jdbc;
+
+import java.sql.Connection;
+
+public class PostgreSource implements Source {
+
+    @Override
+    public Connection getConnection() {
+	return null;
+    }
+
+}
---
+++ ressubt/src/main/java/com/ressubt/jdbc/Source.java
@@ -0,0 +1,8 @@
+package com.ressubt.jdbc;
+
+import java.sql.Connection;
+
+public interface Source {
+
+    public Connection getConnection();
+}
