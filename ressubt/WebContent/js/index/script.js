4f2882ae4c8fa500cc7e507281dff68c6b8cda30
satells <msergiost@hotmail.com>
2021-03-16 15:50:24 -0300

    control form

--- ressubt/WebContent/js/index/script.js
+++ ressubt/WebContent/js/index/script.js
@@ -1,14 +1,37 @@
-const iframe = document.querySelector("iframe");
-const anchors = document.querySelectorAll("a[data-index]");
+import { Operation } from "../../js/cadastro/operation.js";
 
 const links = ["/ressubt/control?action=ViewContribuinte", "/ressubt/control?action=ViewFinalidade", "/ressubt/control?action=ViewParticipante", "/ressubt/control?action=ViewProduto"];
 
 const menuNav = document.querySelector("nav");
 
-menuNav.addEventListener("click", (event) => {
-    let anchor = event.target;
+const iframe = document.querySelector("iframe");
+const anchors = document.querySelectorAll("a[data-index]");
 
-    let index = parseInt(anchor.getAttribute("data-index"));
+const operations = document.querySelector(".operation");
+
+const operationx = new Operation(document);
+
+const eventoBotoes = (event) => {
+    let nomeClasse = event.target.className;
+    if (nomeClasse === "novo") {
+        operationx.disableFields(false);
+        operationx.disableButtons();
+    } else if (nomeClasse === "gravar") {
+        operationx.disableFields(true);
+        operationx.disableButtons();
+    } else if (nomeClasse === "cancelar") {
+        operationx.disableFields(true);
+        operationx.disableButtons();
+    } else if (nomeClasse === "pesquisar") {
+    }
+    console.log(nomeClasse);
+};
+
+const selecionarFormulario = (event) => {
+    let anchor = event.target;
+    let data_index = anchor.getAttribute("data-index");
+    let index = parseInt(data_index);
+
     iframe.src = links[index];
 
     for (let i = 0; i < anchors.length; i++) {
@@ -18,12 +41,10 @@ menuNav.addEventListener("click", (event) => {
     if (anchor instanceof HTMLAnchorElement) {
         anchor.className = "active";
     }
+};
 
-    let controls = iframe.contentWindow.document.querySelectorAll("select, input");
-    for (let i = 0; i < controls.length; i++) {
-        console.log(i);
-        console.log(controls[i].disable);
-        console.log((controls[i].disable = false));
-        console.log(controls[i].disable);
-    }
-});
+menuNav.addEventListener("click", selecionarFormulario);
+iframe.onload = () => {
+    operationx.selecionarControleFormulario(iframe);
+};
+operations.addEventListener("click", eventoBotoes);
